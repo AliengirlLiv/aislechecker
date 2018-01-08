34a6f29be98a7c4dcaeb7606a6d083c5cc6f5af3
AliengirlLiv <oliviagracewatkins@gmail.com>
2018-01-08 08:23:51 -0800

    default value, save confirmation, and fixed save position

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -9,7 +9,9 @@ export default class AllItems extends Component {
 
 	constructor(props) {
 		super();
-		this.state = {items: props.allItems};
+		this.state = {
+			items: props.allItems,
+			saved: false};
 	}
 
 	render() {
@@ -21,44 +23,57 @@ export default class AllItems extends Component {
 						<div key={index}>
 							<FormGroup>
 								<ControlLabel>New Item</ControlLabel>
-								<FormControl value={item[0]} onChange={(e) => this.updateItem(e, 0, index)} />
-								<FormControl value={item[1]} onChange={(e) => this.updateItem(e, 1, index)} />
-								<FormControl value={item[2]} onChange={(e) => this.updateItem(e, 2, index)} />
-								<FormControl value={item[3]} onChange={(e) => this.updateItem(e, 3, index)} />
+								<FormControl placeholder="Store" value={item[0]} onChange={(e) => this.updateItem(e, 0, index)} />
+								<FormControl placeholder="Aisle" value={item[1]} onChange={(e) => this.updateItem(e, 1, index)} />
+								<FormControl placeholder="Bin" value={item[2]} onChange={(e) => this.updateItem(e, 2, index)} />
+								<FormControl placeholder="Item" value={item[3]} onChange={(e) => this.updateItem(e, 3, index)} />
 							</FormGroup>
 							<Button onClick={() => this.removeItem(index)}>X</Button>
 						</div>
 					);
 				})}
 				<br/>
-				<Button onClick={() => this.addItem()}>+</Button>
 				<br/>
-				<Button onClick={() => this.save()}>Save</Button>
+				<br/>
+				<br/>
+				<div className="bottom-fixed">
+					<Button onClick={() => this.addItem()}>+</Button>
+					<br/>
+					<Button bsStyle={this.state.saved ? "success" : ""} onClick={() => this.save()}>Save</Button>
+				</div>
 			</div>
 		)
 	}
 
 	save() {
 		firebase.database().ref('items').set({
-			items: this.state.items
-  		});
+			items: this.state.items,
+  		}).then(function() {
+  			this.setState({saved: true});
+  		}.bind(this));
 	}
 
 	removeItem(index) {
 		var items = this.state.items;
 		items.splice(index, 1);
-		this.setState({items:items});
+		this.setState({
+			items: items,
+			saved: false});
 	}
 
 	updateItem(e, fieldIndex, index) {
 		var items = this.state.items;
 		items[index][fieldIndex] = e.target.value;
-		this.setState({items:items});
+		this.setState({
+			items: items,
+			saved: false});
 	}
 
 	addItem() {
 		var items = this.state.items;
 		items.push(["", "", "", ""]);
-		this.setState({items:items});
+		this.setState({
+			items: items,
+			saved: false});
 	}
 }
