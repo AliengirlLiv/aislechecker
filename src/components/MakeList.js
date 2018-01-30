fdcff72f4c9a87340e172fd122d96b1df916c369
AliengirlLiv <oliviagracewatkins@gmail.com>
2018-01-30 13:10:55 -0800

    test db and select from list view

--- src/components/MakeList.js
+++ src/components/MakeList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Button,
+			ButtonGroup,
+			DropdownButton,
+			MenuItem,
 			FormGroup, 
 			FormControl, 
 			ControlLabel } from 'react-bootstrap'
@@ -13,10 +16,11 @@ export default class MakeList extends Component {
 		super();
 		this.state = {
 			value: "",
-			list: [],
 			suggestions: [],
 			formattedList: [],
-			name: ""
+			name: "",
+			isSearch: true,
+			allItems: []
 		}
 	}
 
@@ -30,6 +34,7 @@ export default class MakeList extends Component {
 				});
 			}
 		}
+		this.structureAllItems(props.allItems);
 	}
 
 	render() {
@@ -41,14 +46,32 @@ export default class MakeList extends Component {
 		}
 		return (
 			<div>
-				<Autosuggest
-					suggestions={this.state.suggestions}
-					onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
-					onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
-					getSuggestionValue={this.getSuggestionValue.bind(this)}
-					renderSuggestion={this.renderSuggestion}
-					inputProps={inputProps}
-				/>
+				<br/>
+				<ButtonGroup bsSize="large">
+					<Button bsStyle={this.state.isSearch ? "primary" : "default"} onClick={() => this.setState({isSearch: true})}>Search Bar</Button>
+					<Button bsStyle={this.state.isSearch ? "default" : "primary"} onClick={() => this.setState({isSearch: false})}>List </Button>
+				</ButtonGroup>
+				<br/>
+
+				{this.state.isSearch &&
+					<div>
+						<br/>
+						<Autosuggest
+							suggestions={this.state.suggestions}
+							onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
+							onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
+							getSuggestionValue={this.getSuggestionValue.bind(this)}
+							renderSuggestion={this.renderSuggestion}
+							inputProps={inputProps}
+						/>
+					</div>
+				}
+				{!this.state.isSearch &&
+					<div>
+						<br/>
+						{this.getAllItems()}
+					</div>
+				}
 				<h1>Current List</h1>
 				{this.displayCurrentList()}
 				<FormGroup>
@@ -60,6 +83,103 @@ export default class MakeList extends Component {
 				<Button onClick={() => this.goToShopView()}>Use This List (must save changes first)</Button>
 			</div>
 		)
+	}
+
+	getAllItems() {
+		var itemsList = [];
+		for (var store of this.state.allItems) {
+			if (store.key === this.state.store) {
+				itemsList = this.alphabeticalSort(store);
+			}
+		}
+		return(
+			<div>
+				<DropdownButton bsStyle="success" title={this.state.store ? this.state.store : "Select Store"} id="select-store-dropdown">
+					{this.state.allItems.map((item) => {
+						return(
+						<MenuItem key={item.key} onSelect={() => this.setState({store: item.key})}>{item.key}</MenuItem>
+						);
+					})}
+				</DropdownButton>
+				<br/>
+				<ButtonGroup vertical>
+					{itemsList.map((item) => {
+						return(
+							<Button key={item} onClick={() => this.toggleItem(item, !item[4])} bsStyle={item[4] ? "success" : "default"}>{item[3]}, Aisle {item[1]}, Bin {item[2]}</Button>
+						)
+					})}
+				</ButtonGroup>
+			</div>
+		)
+	}
+
+
+	toggleItem(item, shouldAdd) {
+		if (shouldAdd) {
+			this.addToList(item, this.state.formattedList);
+		} else {
+			this.deleteItem(item);
+		}
+		this.selectItem(item);
+	}
+
+	selectItem(itemData) {
+		var item = itemData[3];
+		var bin = itemData[2];
+		var aisle = itemData[1];
+		var store = itemData[0];
+		var list = this.state.allItems;
+		for (var storeObj of list) {
+			if (storeObj.key === store) {
+				for (var aisleObj of storeObj.val) {
+					if (aisleObj.key === aisle) {
+						for (var binObj of aisleObj.val) {
+							if (binObj.key === bin) {
+								for (var itemObj of binObj.val) {
+									if (itemObj.key === item) {
+										itemObj.val = !itemObj.val
+									}
+								}
+								this.setState({allItems: list});
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	//TODO: At some point, don't keep sorting literally every time you re-render the page.
+	alphabeticalSort(store) {
+		var alphaList = [];
+		for (var aisle of store.val) {
+			for (var bin of aisle.val) {
+				for (var item of bin.val) {
+					alphaList.push([store.key, aisle.key, bin.key, item.key, item.val]);
+				}
+			}
+		}
+		alphaList.sort(this.alphaSort);
+		return alphaList;
+	}
+
+	alphaSort(a, b) {
+		if (a[3] < b[3]) {
+			return -1;
+		}
+		if (a[3] > b[3]) {
+			return 1;
+		}
+		return 0;
+	}
+
+	structureAllItems(allItems) {
+		var newAllItems = [];
+		allItems.map(function(item) {
+			this.addToList(item, newAllItems);
+		}.bind(this));
+		this.setState({allItems: newAllItems});
 	}
 
 	saveList() {
@@ -89,7 +209,7 @@ export default class MakeList extends Component {
 											return(
 												<div key={bin.key}>
 												{bin.val.map((item, index) => {
-													return(<div key={index}>{item + " (Bin " + bin.key + ")"}<Button onClick={() => this.deleteItem(store.key, aisle.key, bin.key, item)}>x</Button><br/></div>);
+													return(<div key={index}>{item.key + " (Bin " + bin.key + ")"}<Button onClick={() => this.toggleItem([store.key, aisle.key, bin.key, item.key], false)}>x</Button><br/></div>);
 												})}
 												</div>
 											)
@@ -104,7 +224,11 @@ export default class MakeList extends Component {
 		)
 	}
 
-	deleteItem(store, aisle, bin, item) {
+	deleteItem(itemData) {
+		const item = itemData[3];
+		const bin = itemData[2];
+		const aisle = itemData[1];
+		const store = itemData[0];
 		var list = this.state.formattedList;
 		for (var storeObj of list) {
 			if (storeObj.key === store) {
@@ -112,8 +236,12 @@ export default class MakeList extends Component {
 					if (aisleObj.key === aisle) {
 						for (var binObj of aisleObj.val) {
 							if (binObj.key === bin) {
-								const itemIndex = binObj.val.indexOf(item);
-								binObj.val.splice(itemIndex, 1);
+								for (var itemObj of binObj.val) {
+									if (itemObj.key === item) {
+										const itemIndex = binObj.val.indexOf(itemObj);
+										binObj.val.splice(itemIndex, 1);
+									}
+								}
 								if (binObj.val.length === 0) {
 									const binIndex = aisleObj.val.indexOf(binObj);
 									aisleObj.val.splice(binIndex, 1);
@@ -163,16 +291,15 @@ export default class MakeList extends Component {
 	}
 
 	getSuggestionValue(suggestion) {
-		this.addToList(suggestion);
+		this.toggleItem(suggestion, true);
 		return suggestion[3];
 	}
 
-	addToList(item) {
+	addToList(item, list) {
 		var store = item[0];
 		var aisle = item[1];
 		var bin = item[2];
 		var name = item[3];
-		var list = this.state.formattedList;
 		for (var s of list) {
 			var foundStore = false;
 			if (s.key === store) {
@@ -185,24 +312,24 @@ export default class MakeList extends Component {
 						for (var b of a.val) {
 							if (b.key === bin) {
 								foundBin = true;
-								b.val.push(name);
+								b.val.push({key:name, val: false});
 								break;
 							}
 						}
 						if (!foundBin) {
-							a.val.push({key: bin, val:[name]});
+							a.val.push({key: bin, val:[{key:name, val: false}]});
 						}	
 						break;
 					}
 				}
 				if (!foundAisle) {
-					s.val.push({key: aisle, val: [{key: bin, val:[name]}]});
+					s.val.push({key: aisle, val: [{key: bin, val:[{key:name, val: false}]}]});
 				}
 				break;
 			}
 		}
 		if (!foundStore) {
-			list.push({key: store, val: [{key: aisle, val: [{key: bin, val: [name]}]}]});
+			list.push({key: store, val: [{key: aisle, val: [{key: bin, val: [{key:name, val: false}]}]}]});
 		}
 	}
 
--- src/index.js
+++ src/index.js
@@ -6,14 +6,28 @@ import registerServiceWorker from './registerServiceWorker';
 import * as firebase from 'firebase';
 
  // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBMbooCW9LSMZgAgA3VGOVBVnCRO1_0v2I",
-  authDomain: "aislechecker-3d363.firebaseapp.com",
-  databaseURL: "https://aislechecker-3d363.firebaseio.com",
-  projectId: "aislechecker-3d363",
-  storageBucket: "",
-  messagingSenderId: "1095924234727"
-};
+
+if (~window.location.href.indexOf('localhost')) {
+	// Test db
+	var config = {
+		apiKey: "AIzaSyAC6HY8GOrpKXz9UMOql22KuL-xq7c4OgU",
+		authDomain: "aislechecker-test.firebaseapp.com",
+		databaseURL: "https://aislechecker-test.firebaseio.com",
+		projectId: "aislechecker-test",
+		storageBucket: "",
+		messagingSenderId: "44576906425"
+  };
+} else {
+	// Real db
+	var config = {
+		apiKey: "AIzaSyBMbooCW9LSMZgAgA3VGOVBVnCRO1_0v2I",
+		authDomain: "aislechecker-3d363.firebaseapp.com",
+		databaseURL: "https://aislechecker-3d363.firebaseio.com",
+		projectId: "aislechecker-3d363",
+		storageBucket: "",
+		messagingSenderId: "1095924234727"
+	};
+}
 firebase.initializeApp(config);
 console.log("ITS WORKING");
 
